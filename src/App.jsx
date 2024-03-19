3bc7de58d1367747a3ff0b682d6e9d8bb9166237
nazmul-nhb <nazmulnhb007@yahoo.com>
2024-03-19 13:16:03 +0600

    added PriceOptions and PriceOption components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
 import NavBar from './components/NavBar/NavBar'
+import PriceOptions from './components/PriceOptions/PriceOptions'
 // import DaisyNavBar from './DaisyNavBar/DaisyNavBar'
 
 function App() {
+
+  
 
   return (
     <>
       {/* <DaisyNavBar></DaisyNavBar> */}
       <NavBar></NavBar>
+      <PriceOptions></PriceOptions>
     </>
   )
 }
