bf6f8fefc3bf5d2f8382bae3e637befca9e44a51
nazmul-nhb <nazmulnhb007@yahoo.com>
2024-03-19 15:39:29 +0600

    added loader spinner

--- src/components/Phones/Phones.jsx
+++ src/components/Phones/Phones.jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, Tooltip } from 'recharts';
+import { DNA } from 'react-loader-spinner';
 
 const Phones = () => {
 
     const [phones, setPhones] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         // fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
@@ -24,19 +26,28 @@ const Phones = () => {
                     return obj;
                 })
                 setPhones(phonesWithFakeData);
+                setIsLoading(false);
             });
 
     }, [])
 
     return (
         <div>
+            {isLoading && <DNA
+                visible={true}
+                height="80"
+                width="80"
+                ariaLabel="dna-loading"
+                wrapperStyle={{}}
+                wrapperClass="dna-wrapper"
+            />}
             <h3 className="text-3xl">Phones: {phones.length}</h3>
-                <BarChart width={1400} height={400} data={phones}>
-                    <Bar dataKey="price" fill="blue" />
-                    <XAxis dataKey={'name'}></XAxis>
-                    <YAxis></YAxis>
-                    <Tooltip></Tooltip>
-                </BarChart>
+            <BarChart width={1400} height={400} data={phones}>
+                <Bar dataKey="price" fill="blue" />
+                <XAxis dataKey={'name'}></XAxis>
+                <YAxis></YAxis>
+                <Tooltip></Tooltip>
+            </BarChart>
         </div>
     );
 };
